05077979d2dc29d0f76e50b2ccc6cd2252d8e708
OrkhanBV <babaev.or@yandex.ru>
2020-12-16 15:32:50 +0300

    Upload work as a ApiController throw the HttpPost

--- WebApiOrkhan/WebApiOrkhan/Controllers/UploadNewMaterialController.cs
+++ WebApiOrkhan/WebApiOrkhan/Controllers/UploadNewMaterialController.cs
@@ -17,8 +17,8 @@ using File = WebApiOrkhan.Data.Models.File;
 
 namespace WebApiOrkhan.Controllers
 {
-    /*[ApiController]
-    [Route("/Material/Upload")]*/
+    [ApiController]
+    [Route("/Material/Upload")]
     public class UploadMaterialController : Controller
     {
         //private IHostingEnvironment _env;
@@ -36,7 +36,7 @@ namespace WebApiOrkhan.Controllers
         
         public IActionResult Indexupload() => View();
         
-        /*[HttpPost]*/
+        [HttpPost]
         public IActionResult FileInModel(FormForMaterials FormForMaterials)
         {
             //заменить на кейсы 
@@ -83,79 +83,7 @@ namespace WebApiOrkhan.Controllers
                 FormForMaterials.File.CopyTo(fileStream);
             }
             return RedirectToAction("Indexupload");
+            
         }
     }
 }
-
-/*I'm surprised that a lot of you seem to want to save files on the server.
- Solution to keep everything in memory is as follows:
-
-
-Пример пост запроса Вариант 1
-[HttpPost("api/upload")]
-public async Task<IHttpActionResult> Upload()
-{
-if (!Request.Content.IsMimeMultipartContent())
-throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType); 
-
-var provider = new MultipartMemoryStreamProvider();
-await Request.Content.ReadAsMultipartAsync(provider);
-    foreach (var file in provider.Contents)
-{
-    var filename = file.Headers.ContentDisposition.FileName.Trim('\"');
-    var buffer = await file.ReadAsByteArrayAsync();
-    //Do whatever you want with filename and its binary data.
-}
-
-return Ok();
-}*/
-
-/*
- Вариант 2
- 
- public HttpResponseMessage Post()
-{
-var httpRequest = HttpContext.Current.Request;
-    if (httpRequest.Files.Count < 1)
-{
-    return Request.CreateResponse(HttpStatusCode.BadRequest);
-}
-
-foreach(string file in httpRequest.Files)
-{
-    var postedFile = httpRequest.Files[file];
-    var filePath = HttpContext.Current.Server.MapPath("~/" + postedFile.FileName);
-    postedFile.SaveAs(filePath);
-    // NOTE: To store in memory use postedFile.InputStream
-}
-
-return Request.CreateResponse(HttpStatusCode.Created);
-}*/
-
-/*
- Вариант 3 с Формой
- 
- [HttpPost("UploadFiles")]
-public async Task<IActionResult> Post(List<IFormFile> files)
-{
-long size = files.Sum(f => f.Length);
-
-// full path to file in temp location
-var filePath = Path.GetTempFileName();
-
-    foreach (var formFile in files)
-{
-    if (formFile.Length > 0)
-    {
-        using (var stream = new FileStream(filePath, FileMode.Create))
-        {
-            await formFile.CopyToAsync(stream);
-        }
-    }
-}
-
-// process uploaded files
-// Don't rely on or trust the FileName property without validation.
-
-return Ok(new { count = files.Count, size, filePath});
-}*/

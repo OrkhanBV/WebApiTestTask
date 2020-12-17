29e6d97f547bf60550e07b512bfda17fc1147967
OrkhanBV <babaev.or@yandex.ru>
2020-12-17 21:41:24 +0300

    logging middleware almost is done

--- WebApiOrkhan/WebApiOrkhan/Middleware/RequestResponseLoggingMiddlewareExtensions.cs
+++ WebApiOrkhan/WebApiOrkhan/Middleware/RequestResponseLoggingMiddlewareExtensions.cs
@@ -1,5 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 
+/*
+ Links about Logging Meddleware--->
+ https://elanderson.net/2019/12/log-requests-and-responses-in-asp-net-core-3/
+ https://metanit.com/sharp/aspnet5/2.10.php
+ https://blog.elmah.io/asp-net-core-request-logging-middleware/
+*/
+
 namespace WebApiOrkhan.Middleware
 {
     

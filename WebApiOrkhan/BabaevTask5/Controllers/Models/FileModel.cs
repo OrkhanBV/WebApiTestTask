1692f0f2d506c41591d383f581b0903125399509
OrkhanBV <babaev.or@yandex.ru>
2021-01-27 00:00:23 +0300

    start again

--- WebApiOrkhan/BabaevTask5/Controllers/Models/FileModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BabaevTask5.Controllers.Models
-{
-    public class FileModel
-    {
-        public string fileType;
-        public string filePath;
-        public string fileName;
-    }
-}
--- WebApiOrkhan/BabaevTask5/ViewModels/EditUserViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BabaevTask5.ViewModels
-{
-    public class EditUserViewModel
-    {
-        public string Id { get; set; }
-        public string Email { get; set; }
-        /*public int Year { get; set; }*/
-    }
-}
